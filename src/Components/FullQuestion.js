89e8648491074126208509e8734561693e4f2abd
Faraaz-Beyabani <beyabanifaraaz@gmail.com>
2019-02-20 12:22:01 -0600

    Transition from HTML to MUI Card

--- src/Components/FullQuestion.js
+++ src/Components/FullQuestion.js
@@ -1,32 +1,39 @@
 import React, { Component } from "react";
 import "../FullQuestion.css";
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
 import ArrowUpward from '@material-ui/icons/ArrowUpward';
+import QuestionAnswer from '@material-ui/icons/QuestionAnswer';
+
 import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import CardActions from '@material-ui/core/CardActions';
+import Avatar from '@material-ui/core/Avatar';
+
 import orange from '@material-ui/core/colors/orange';
-
-const theme = createMuiTheme({
-  palette: {
-    primary: orange
-}});
-
+import { IconButton, withStyles } from "@material-ui/core";
 
 class FullQuestion extends Component {
+  
   render() {
-    return <div id="fullQuestion">
-              <div> 
-                <h4 id="questionTitle">
-                <MuiThemeProvider theme={theme}>
-                  <Button id="upVote" variant="contained" color="primary" >
-                    1024
-                    <ArrowUpward/>
-                  </Button>
-                </MuiThemeProvider>
-                  What is the Pythagorean Theorem?
-                </h4>
-              </div>
-              <p id="questionBody">And how can I use it to create an AI in HTML??</p>
-            </div>;
+    const { classes } = this.props;
+    return <Card>
+              <CardHeader id="cardHeader"
+                          avatar={<Avatar rounded> <QuestionAnswer></QuestionAnswer> </Avatar>}
+                          action={<IconButton><ArrowUpward></ArrowUpward></IconButton>}  
+                          title="What is the Pythagorean Theorem?" 
+                          subheader="num_upvotes">
+                
+              </CardHeader>
+              <CardContent>
+                <Typography id="questionText" component="p">
+                  And how can I use it to create an AI in HTML??
+                </Typography> 
+              </CardContent>
+            </Card>;
   }
 }
 
