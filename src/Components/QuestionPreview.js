d2802141b68dc83fa3df8f080f5e84c62016dff1
sujunglee <sujunglee2020@u.northwestern.edu>
2019-02-20 16:03:29 -0600

    Clean up package-lock

--- src/Components/QuestionPreview.js
+++ src/Components/QuestionPreview.js
@@ -1,25 +1,37 @@
 import React, { Component } from "react";
 import "../QuestionPreview.css";
 
-import ArrowUpward from '@material-ui/icons/ArrowUpward';
-import QuestionAnswer from '@material-ui/icons/QuestionAnswer';
+import ArrowUpward from "@material-ui/icons/ArrowUpward";
+import QuestionAnswer from "@material-ui/icons/QuestionAnswer";
 
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import Avatar from '@material-ui/core/Avatar';
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import Avatar from "@material-ui/core/Avatar";
 
 import { IconButton } from "@material-ui/core";
 
 class QuestionPreview extends Component {
   render() {
-    return <Card>
-            <CardHeader id="cardHeader"
-                        avatar={<Avatar rounded> <QuestionAnswer></QuestionAnswer> </Avatar>}
-                        action={<IconButton><ArrowUpward></ArrowUpward></IconButton>}  
-                        title="question_title" 
-                        subheader="num_upvotes">
-            </CardHeader> 
-           </Card>;
+    return (
+      <Card>
+        <CardHeader
+          id="cardHeader"
+          avatar={
+            <Avatar rounded>
+              {" "}
+              <QuestionAnswer />{" "}
+            </Avatar>
+          }
+          action={
+            <IconButton>
+              <ArrowUpward />
+            </IconButton>
+          }
+          title="question_title"
+          subheader="num_upvotes"
+        />
+      </Card>
+    );
   }
 }
 
