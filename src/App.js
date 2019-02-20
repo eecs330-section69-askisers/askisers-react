086f97a0e2c84fc3b4d298d6d09ca09b3123764d
Faraaz-Beyabani <beyabanifaraaz@gmail.com>
2019-02-20 13:51:43 -0600

    Convert SideBar to React component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
-import "./App.css";
-import QuestionPreview from "./Components/QuestionPreview";
+import "./SideBar.css";
+import SideBar from "./Components/SideBar";
 
 class App extends Component {
   render() {
@@ -12,15 +12,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Yay
-          </a>
-          <QuestionPreview />
+          <SideBar/>
         </header>
       </div>
     );
--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -1,8 +1,98 @@
 import React, { Component } from "react";
+import "../SideBar.css"
+import {IconButton, Icon} from "@material-ui/core"
+import {Search, Add} from "@material-ui/icons"
 
 class SideBar extends Component {
   render() {
-    return <div>Hello</div>;
+    return <div id="sidebarsteve">
+		          <div className="topnav">
+  			        <input type="text" placeholder="Find a question or topic.."/>
+                  <IconButton><Search/></IconButton>
+		          </div>
+		          <br/>
+		          <button className="button">Create a new post!</button>
+		          <br/><br/>
+		          <div id="question-title">Questions</div>
+		          <br/><br/><br/>
+		          <div className="question">
+		          	<div className="row">
+  				        <div className="columnA">
+					          <IconButton className="upvote"><Add/></IconButton>
+					          <br/>
+					          <p>13</p>
+                  </div>
+ 				          <div className="columnB">
+					          How do you perform the Pythagorean Theorem? What does it do?
+				          </div>
+		            </div>
+		          </div>
+              <br/><br/>
+				      <div className="question">
+		          	<div className="row">
+  				        <div className="columnA">
+                  <IconButton className="upvote"><Add/></IconButton>
+					          <br/>
+					          <p>10</p>
+				          </div>
+ 				          <div className="columnB">
+					          What do I need to learn how to add?
+                  </div>
+			          </div>
+		          </div>
+		          <br/><br/>
+				      <div className="question">
+			          <div className="row">
+                  <div className="columnA">
+                  <IconButton className="upvote"><Add/></IconButton>
+					          <br/>
+					          <p>7</p>
+				          </div>
+ 				          <div className="columnB">
+					          If I have an isosceles triangle, what do I do with my other triangles?
+				          </div>
+		          	</div>
+		          </div>
+	          	<br/><br/>
+				      <div className="question">
+			          <div className="row">
+  				        <div className="columnA">
+                  <IconButton className="upvote"><Add/></IconButton>
+					          <br/>
+					          <p>5</p>
+                  </div>
+ 				          <div className="columnB">
+					          Can someone please explain long division?
+				          </div>
+		          	</div>
+	          	</div>
+		          <br/><br/>
+				      <div className="question">
+		          	<div className="row">
+  				        <div className="columnA">
+                  <IconButton className="upvote"><Add/></IconButton>
+                    <br/>
+				        	  <p>2</p>
+				          </div>
+ 				          <div className="columnB">
+					          How do I carry a zero?
+				          </div>
+			          </div>
+	          	</div>
+		          <br/><br/>
+				      <div className="question">
+		          	<div className="row">
+  				        <div className="columnA">
+                  <IconButton className="upvote"><Add/></IconButton>
+					          <br/>
+					          <p>0</p>
+				          </div>
+ 				          <div className="columnB">
+					          Is there any other way for me to find the integral between two points on a parametric equation?
+				          </div>
+		          	</div>
+		          </div>
+		        </div>;
   }
 }
 
