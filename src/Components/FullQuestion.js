535a48a1b7a9f3f0e362cbfcef418b1b94228d55
Faraaz-Beyabani <beyabanifaraaz@gmail.com>
2019-02-20 14:59:00 -0600

    Cleaned up unnecessary stylings and imports

--- src/Components/FullQuestion.js
+++ src/Components/FullQuestion.js
@@ -1,25 +1,20 @@
 import React, { Component } from "react";
 import "../FullQuestion.css";
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 import ArrowUpward from '@material-ui/icons/ArrowUpward';
 import QuestionAnswer from '@material-ui/icons/QuestionAnswer';
 
-import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import CardActions from '@material-ui/core/CardActions';
 import Avatar from '@material-ui/core/Avatar';
 
-import orange from '@material-ui/core/colors/orange';
-import { IconButton, withStyles } from "@material-ui/core";
+import { IconButton } from "@material-ui/core";
 
 class FullQuestion extends Component {
   
   render() {
-    const { classes } = this.props;
     return <Card>
               <CardHeader id="cardHeader"
                           avatar={<Avatar rounded> <QuestionAnswer></QuestionAnswer> </Avatar>}
