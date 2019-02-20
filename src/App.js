a0e51608ed9f2b9c9a12cb6ab3b9d8227a672f2f
sujunglee <sujunglee2020@u.northwestern.edu>
2019-02-20 16:30:33 -0600

    Making sure all of the components actually appear

--- src/App.js
+++ src/App.js
@@ -4,27 +4,18 @@ import "./App.css";
 import AnswerEntry from "./Components/AnswerEntry";
 import "./SideBar.css";
 import SideBar from "./Components/SideBar";
+import FullQuestion from "./Components/FullQuestion";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and press alt f4 to save changes :).
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Yay
-          </a>
-          <AnswerEntry/>
-          <SideBar/>
-        </header>
+      <div>
+        <div className="Sidebar-Wrapper">
+          <SideBar />
+        </div>
+        <div className="Question-Wrapper">
+          <FullQuestion />
+        </div>
       </div>
     );
   }
--- src/Components/Answer.js
+++ src/Components/Answer.js
@@ -1,37 +1,47 @@
 import React, { Component } from "react";
-import "../Answer.css"
+import "../Answer.css";
 
-import ArrowUpward from '@material-ui/icons/ArrowUpward';
-import ArrowDownward from '@material-ui/icons/ArrowDownward';
-import QuestionAnswer from '@material-ui/icons/QuestionAnswer';
+import ArrowUpward from "@material-ui/icons/ArrowUpward";
+import ArrowDownward from "@material-ui/icons/ArrowDownward";
+import QuestionAnswer from "@material-ui/icons/QuestionAnswer";
 
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Typography from '@material-ui/core/Typography';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-
-
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
 
 class Answer extends Component {
   render() {
-    return <Card>
-              <CardHeader id="cardHeader"
-                          avatar={<Avatar rounded> <QuestionAnswer></QuestionAnswer> </Avatar>}
-                          subheader="num_upvotes">
-              </CardHeader>
-              <CardContent>
-                <Typography id="answerText" component="p">
-                  answer_body
-                </Typography> 
-              </CardContent>
-              <CardActions disableActionSpacing>
-                <IconButton><ArrowDownward></ArrowDownward></IconButton>
-                <IconButton><ArrowUpward></ArrowUpward></IconButton>
-              </CardActions>
-            </Card>;
+    return (
+      <Card>
+        <CardHeader
+          id="cardHeader"
+          avatar={
+            <Avatar rounded>
+              {" "}
+              <QuestionAnswer />{" "}
+            </Avatar>
+          }
+          subheader="num_upvotes"
+        />
+        <CardContent>
+          <Typography id="answerText" component="p">
+            answer_body
+          </Typography>
+        </CardContent>
+        <CardActions disableActionSpacing>
+          <IconButton>
+            <ArrowDownward />
+          </IconButton>
+          <IconButton>
+            <ArrowUpward />
+          </IconButton>
+        </CardActions>
+      </Card>
+    );
   }
 }
 
